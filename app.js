03112431b0b42cbbbade9fa9b6f02ed1b567bdf7
cmamani <mamanimezacarlos@gmail.com>
2021-11-21 17:36:54 -0300

    c9: mongoDB - connect to a database

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
 
 // express app
 const app = express();
 
+// connect to mongodb
+const dbURI = 'mongodb+srv://netninja:callefalsa123@nodetuts.t1tjg.mongodb.net/nodetuts?retryWrites=true&w=majority';
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then((result) => app.listen(3000))
+    .catch((err) => console.log(err));
+
 // register view engine
 app.set('view engine', 'ejs');
 
-// listen for requests
-app.listen(3000);
 
 // middleware & static files
 app.use(express.static('public'));
